7fffe93e0bd8de8215200febf5aa1249618770bb
Laura-Nyaaga <nyaagaaluoch@gmail.com>
2024-06-09 20:12:20 +0300

    added registration activities

--- app/src/main/java/com/akirachix/mentorconnect/MainActivity.kt
+++ app/src/main/java/com/akirachix/mentorconnect/MainActivity.kt
@@ -8,17 +8,89 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.akirachix.mentorconnect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val textView3 = findViewById<TextView>(R.id.textView3)
-        textView3.setOnClickListener {
-            val intent = Intent( this, SignInPage::class.java)
+        binding.textView3.setOnClickListener {
+            val intent = Intent(this, SignInPage::class.java)
             startActivity(intent)
         }
+        binding.btnRegister.setOnClickListener {
+            validateRegistration()
+        }
 
+
+    }
+
+    fun validateRegistration() {
+        clearErrors()
+        var formError = false
+        val firstName = binding.etFirstName.text.toString()
+        if (firstName.isBlank()) {
+            formError = true
+            binding.tilFirstName.error = getString(R.string.first_name_is_required)
+        }
+
+        val lastName = binding.etLastName.text.toString()
+        if (lastName.isBlank()) {
+            formError = true
+            binding.tilLastName.error = getString(R.string.last_name_is_required)
+        }
+
+        val email = binding.etEmail.text.toString()
+        if (email.isBlank()) {
+            formError = true
+            binding.tilEmail.error = getString(R.string.email_is_required)
+        }
+
+        val code = binding.etCodehiveId.text.toString()
+        if (code.isBlank()) {
+            formError = true
+            binding.tilCodehiveId.error = getString(R.string.codehive_id_is_required)
+        }
+
+
+        val username = binding.etUsername.text.toString()
+        if (username.isBlank()) {
+            formError = true
+            binding.tilUsername.error = getString(R.string.username_is_required)
+        }
+
+
+        val password = binding.etPassword.text.toString()
+        if (password.isBlank()) {
+            formError = true
+            binding.tilPassword.error = getString(R.string.password_is_required)
+        }
+
+
+        val confirmPassWord = binding.etConfirmPassword.text.toString()
+        if (confirmPassWord.isBlank()) {
+            formError = true
+            binding.tilConfirmPassword.error = getString(R.string.password_confirmation_is_required)
+        }
+
+
+        if (password != confirmPassWord) {
+            formError = true
+            binding.tilConfirmPassword.error =
+                getString(R.string.password_and_confirmation_do_not_match)
+        }
+    }
+
+    fun clearErrors() {
+        binding.tilFirstName.error = null
+        binding.tilLastName.error = null
+        binding.tilEmail.error = null
+        binding.tilCodehiveId.error = null
+        binding.tilUsername.error = null
+        binding.tilPassword.error = null
+        binding.tilConfirmPassword.error = null
     }
 }
--- app/src/main/java/com/akirachix/mentorconnect/SignInPage.kt
+++ app/src/main/java/com/akirachix/mentorconnect/SignInPage.kt
@@ -2,21 +2,51 @@ package com.akirachix.mentorconnect
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.akirachix.mentorconnect.databinding.ActivitySignInPageBinding
 
 class SignInPage : AppCompatActivity() {
+    lateinit var binding: ActivitySignInPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in_page)
-        val textView4 = findViewById<TextView>(R.id.textView4)
-        textView4.setOnClickListener {
-            val intent = Intent( this, MainActivity::class.java)
+        binding = ActivitySignInPageBinding.inflate(layoutInflater)
+        this.setContentView(binding.root)
+
+        binding.textView4.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        binding.btnLogin.setOnClickListener {
+            validateLogIn()
+        }
+
+    }
+
+    fun validateLogIn() {
+        deleteErrors()
+        var pageError = false
+        val name = binding.etUserLogIn.text.toString()
+        if (name.isBlank()) {
+            pageError = true
+            binding.tilUserLogIn.error = getString(R.string.username_required)
+        }
+
+        val passwordLogin = binding.etPasswordLogIn.text.toString()
+        if (passwordLogin.isBlank()) {
+            pageError = true
+            binding.tilPasswordLogIn.error = getString(R.string.password_required)
+        }
+
+    }
+
+    fun deleteErrors() {
+        binding.tilUserLogIn.error = null
+        binding.tilPasswordLogIn.error = null
     }
 }
